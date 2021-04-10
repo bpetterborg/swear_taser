613fbce7580cba78c21f7b28da933cab6b8a75a5
bpetterborg <ben@ben-p.dev>
2021-04-09 23:54:43 -0400

    Completed this, still need to test

--- APP.ino
+++ APP.ino
@@ -6,12 +6,32 @@
  *  
  */
 
+int incomingData;
+int TASING_TIME = 1000;
+
 void setup() 
 {
-    
+    Serial.begin(9600);
+    pinMode(11, OUTPUT);    // status led
+    pinMode(12, OUTPUT);    // relay (for taser)
+
+    digitalWrite(11, HIGH);
+    delay(10);
+    Serial.write("Connected to Arduino");
+    digitalWrite(11, LOW);
 }
 
 void loop() 
 {
-    
+    while (Serial.available())
+    {
+        incomingData = Serial.read();
+        if (incomingData == 1)
+        {
+            digitalWrite(12, HIGH);     // taser on
+            Serial.write("Tasing...");  
+            delay(TASING_TIME);         // wait for TASING_TIME ms
+            digitalWrite(12, LOW);      // taser off
+        }
+    }
 }
