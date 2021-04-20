d75b19958e83773d1a51122277f126a05fd93b3f
bpetterborg <ben.petterborg@gmail.com>
2021-04-20 17:21:19 -0400

    Removed old code

--- main.py
+++ main.py
@@ -20,35 +20,7 @@ arduino = serial.Serial(PORT, 9600, timeout=.1)
 
 swears = open('swears.txt','r').read().split()
 
-
-
-# connect to arduino over serial, detect swears
-#while True:
-#	
-#	# display messages from arduino
-	# data = arduino.readline()[:-2]	# [:-2] removes line-endings
-
-	# # speech recognition
-	# with mic as source:
-
-	# 	# adjust for background noise
-	# 	r.adjust_for_ambient_noise(source)
-# 
-#		# listen to source
-#		audio = r.listen(source)
-#		# run the recognizer on it
-#		words = r.recognize_google(audio)
-#		print(words) # print what user says
-#
-#		if any(element in swears for element in words.split()):
-#			print('Detected swear, tasing')
-#			arduino.write(1)
-#
-#		else:
-#			print('No swears detected.')
-
-
-
+# detecting speech
 try:
 	# adjust for background noise
 	with mic as source: 
@@ -73,26 +45,53 @@ try:
 
 			# print it as unicode TODO: Remove this
 			#if str is bytes:  # this one for python2
-			#	print(u"You said {}".format(value).encode("utf-8"))
+			#	print(u"{}".format(value).encode("utf-8"))
 			
 			# this one is for python3
 			#else:
-			print("{}".format(value))
+			print("{}".format(value)) # may need to remove the {} and .format thing.
 
-			print(value)
+			print(value) # this might not be necessary, test this
 
-			# determine if any of the words are swears TODO: Add thing to read swears from file
+			# compare value string with swears list
 			if any(element in swears for element in value.split()): # may need to do extra processing
 				print('Detected Keyword. Tasing.')
-				arduino.write(1) # send a 1 to arduino
+				arduino.write(1) # send a 1 to arduino TODO: Test this, some with serial console
 
 		
 		except sr.UnknownValueError:
 			print('Unrecognized. Try again.')
 		
 		except sr.RequestError as error:
-			print("Unable to get results from Google Cloud Speech Recognition Library; {0}".format(error))
+			print("Unable to get results (sr.RequestError) {0}".format(error))
 
 # ctrl+c to exit
 except KeyboardInterrupt:
 	pass
+
+
+# Old stuff, TODO: Delete this
+# connect to arduino over serial, detect swears
+#while True:
+#	
+#	# display messages from arduino
+	# data = arduino.readline()[:-2]	# [:-2] removes line-endings
+
+	# # speech recognition
+	# with mic as source:
+
+	# 	# adjust for background noise
+	# 	r.adjust_for_ambient_noise(source)
+# 
+#		# listen to source
+#		audio = r.listen(source)
+#		# run the recognizer on it
+#		words = r.recognize_google(audio)
+#		print(words) # print what user says
+#
+#		if any(element in swears for element in words.split()):
+#			print('Detected swear, tasing')
+#			arduino.write(1)
+#
+#		else:
+#			print('No swears detected.')
